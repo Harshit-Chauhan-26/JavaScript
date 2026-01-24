5b69783f47b1c1ea5fccc6423ad435e82c0ef574
Harshit Chauhan <businessharshit26@gmail.com>
2026-01-24 13:52:00 +0530

    Add files via upload

---
+++ JavaScript/01_BASICS/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello"); we are using node.js, not browsers
+
+console.log(3
+     +
+      3); // code readability should be high
+
+console.log("Harshit");
+
+let name = "harshit";
+let age = 18;
+let isloggedIn = false;
+let state; // undefinded
+let state1 = null; // null
+
+// number => 2 to pwer 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
